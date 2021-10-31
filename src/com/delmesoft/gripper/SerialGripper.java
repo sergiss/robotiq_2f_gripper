d36755fd152993f65f5fc3ff70167ffc066ef14b
Sergio Soriano <sergi.ss4@gmail.com>
2021-10-31 14:27:16 +0100

    Update

--- src/com/delmesoft/gripper/SerialGripper.java
+++ src/com/delmesoft/gripper/SerialGripper.java
@@ -9,9 +9,9 @@ public class SerialGripper implements Gripper {
 
 	private final String port;
 	private int baudRate;
-	
+
 	private SyncSerialPort serialPort;
-	
+
 	public SerialGripper(String port) {
 		this(port, 115200);
 	}
@@ -35,43 +35,46 @@ public class SerialGripper implements Gripper {
 	}
 
 	public synchronized void restart() throws Exception {
-		
+
 		deactivate();
 		activate();
-		
-		while(true) {
-			byte[] data = { 0x09, 0x03, 0x07, (byte) 0xD0, 0x00, 0x01, (byte) 0x85, (byte) 0xCF }; // Read Gripper status until the activation is completed
+
+		while (true) {
+			// Read Gripper status until the activation is completed
+			byte[] data = { 0x09, 0x03, 0x07, (byte) 0xD0, 0x00, 0x01, (byte) 0x85, (byte) 0xCF };
 			serialPort.writeBytes(data);
 			data = new byte[7];
 			serialPort.readBytes(data);
-			if(Arrays.equals(data, ACTIVATION_COMPLETE)) {
+			if (Arrays.equals(data, ACTIVATION_COMPLETE)) {
 				break; // OK
-			} else if(!Arrays.equals(data, ACTIVATION_PENDING)) {
+			} else if (!Arrays.equals(data, ACTIVATION_PENDING)) {
 				throw new RuntimeException("Restart error");
-			} 
+			}
 		}
-		
+
 	}
-	
+
 	private boolean check() throws Exception {
 		byte[] data = new byte[8];
 		serialPort.readBytes(data);
 		boolean result = Arrays.equals(data, RESPONSE_ACK);
 		return result;
 	}
-	
+
 	private synchronized void deactivate() throws Exception {
-		byte[] data = { 0x09, 0x10, 0x03, (byte) 0xE8, 0x00, 0x03, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x73, 0x30 }; // Deactivate gripper
+		// Deactivate gripper
+		byte[] data = { 0x09, 0x10, 0x03, (byte) 0xE8, 0x00, 0x03, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x73, 0x30 };
 		serialPort.writeBytes(data);
-		if(!check()) {
+		if (!check()) {
 			throw new RuntimeException("Deactivate error");
 		}
 	}
 
 	private synchronized void activate() throws Exception {
-		byte[] data = { 0x09, 0x10, 0x03, (byte) 0xE8, 0x00, 0x03, 0x06, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x72, (byte) 0xE1 }; // Activate gripper
+		// Activate gripper
+		byte[] data = { 0x09, 0x10, 0x03, (byte) 0xE8, 0x00, 0x03, 0x06, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x72, (byte) 0xE1 };
 		serialPort.writeBytes(data);
-		if(!check()) {
+		if (!check()) {
 			throw new RuntimeException("Activate error");
 		}
 	}
@@ -95,31 +98,33 @@ public class SerialGripper implements Gripper {
 		final byte f = (byte) (255.0 * force);
 		pose(p, s, f);
 	}
-	
+
 	@Override
 	public synchronized void pose(byte position, byte speed, byte force) throws Exception {
 		position = (byte) clamp(position & 0xFF, 0, 255);
-		speed    = (byte) clamp(speed    & 0xFF, 0, 255);
-		force    = (byte) clamp(force    & 0xFF, 0, 255);
-        //                09    10    03           E8    00    03    06    09    00    00 
-		byte[] data = { 0x09, 0x10, 0x03, (byte) 0xE8, 0x00, 0x03, 0x06, 0x09, 0x00, 0x00, position, speed, force, 0x00, 0x00};
-		send(data);	
+		speed = (byte) clamp(speed & 0xFF, 0, 255);
+		force = (byte) clamp(force & 0xFF, 0, 255);
+		// 09 10 03 E8 00 03 06 09 00 00
+		byte[] data = { 0x09, 0x10, 0x03, (byte) 0xE8, 0x00, 0x03, 0x06, 0x09, 0x00, 0x00, position, speed, force, 0x00, 0x00 };
+		send(data);
 	}
-	
+
 	private int clamp(int value, int min, int max) {
-		if (value < min) return min;
-		if (value > max) return max;
+		if (value < min)
+			return min;
+		if (value > max)
+			return max;
 		return value;
 	}
 
 	protected synchronized void send(byte[] data) throws Exception {
-		if(isConnected()) {
+		if (isConnected()) {
 			int n = data.length;
 			int[] crc = CRC16.calculateCRC(data, 0, n - 2);
 			data[n - 2] = (byte) crc[0];
 			data[n - 1] = (byte) crc[1];
 			serialPort.writeBytes(data);
-			if(!check()) {
+			if (!check()) {
 				throw new RuntimeException("Send data error");
 			}
 		}
@@ -130,12 +135,12 @@ public class SerialGripper implements Gripper {
 		// 09 03 07 D0 00 03 04 0E
 		byte[] data = { 0x09, 0x03, 0x07, (byte) 0xD0, 0x00, 0x03, 0x04, 0x0E };
 		serialPort.writeBytes(data);
-		
+
 		data = new byte[11];
 		serialPort.readBytes(data);
 		return new State(data);
 	}
-	
+
 	public int getBaudRate() {
 		return baudRate;
 	}
